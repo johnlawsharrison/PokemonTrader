a4a9e7cb7ffca69f9abbb62cd367fc284739e07e
John Laws Harrison <johnlawsharrison@gmail.com>
2016-07-18 05:21:26 -0700

    Set up firebase, testing

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('PokemonTrader', ['ui.bootstrap', 'ui.router']);
+var app = angular.module('PokemonTrader', ['ui.bootstrap', 'ui.router', 'firebase']);
 
 //configure routes
 app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
@@ -10,28 +10,72 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
 			templateUrl: 'partials/tradelist.html',
 			controller: 'TradeListCtrl'
 		})
+		.state('signup', {
+			url: '/signup',
+			templateUrl: 'partials/sign-up.html',
+			controller: 'SignUpCtrl'
+		});
 	$urlRouterProvider.otherwise('/');
+}]);
+
+//controller for navbar (logout and search from any part of the app)
+app.controller('NavCtrl', ['$scope', '$firebaseAuth', function ($scope, $firebaseAuth) {
+	var Auth = $firebaseAuth();
+
+	//respond to changes in auth state
+	Auth.$onAuthStateChanged(function(firebaseUser) {
+		if (firebaseUser) {
+			$scope.uid = firebaseUser.uid;
+		} else {
+			$scope.uid = undefined;
+		}
+	});
+
+	//log out the signed-in user
+	$scope.logout = function () {
+		Auth.$signOut();
+	};
+
 }]);
 
 //controller for trading list
 app.controller('TradeListCtrl', ['$scope', function ($scope) {
 }]);
 
-//service for managing the state of the tradelist
-app.factory('tradelistService', function() {
-	var service = {}
+//controller for sign up/in page
+app.controller('SignUpCtrl', ['$scope', '$firebaseAuth', function ($scope, $firebaseAuth) {
+	var baseRef = firebase.database().ref();
+	var usersRef = baseRef.child('users');
+	var Auth = $firebaseAuth();
 
-	if (localStorage['watchlist'] !== undefined) {
-		service.watchlist = JSON.parse(localStorage.watchlist);
-	} else {
-		service.watchlist = [];
-	}
+	//respond to changes in auth state
+	Auth.$onAuthStateChanged(function(firebaseUser) {
+		if (firebaseUser) {
+			$scope.uid = firebaseUser.uid;
+		} else {
+			$scope.uid = undefined;
+		}
+	});
 
-	service.addPost = function(movie) {
+	//create a new user with firebase
+	$scope.signUp = function () {
+		Auth.$createUserWithEmailAndPassword($scope.email, $scope.password)
+			.then(function(firebaseUser) {
+				console.log('user created: ' + firebaseUser.uid);
+				var userData = {
+					'username': $scope.username
+				};
+				var newUser = usersRef.child(firebaseUser.uid);
+				newUser.set(userData);
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
 	};
 
-	service.removePost = function() {
+	//sign in as the user
+	$scope.signIn = function () {
+		console.log(Auth.$signInWithEmailAndPassword($scope.email, $scope.password));
 	};
 
-	return service;
-});
+}]);
