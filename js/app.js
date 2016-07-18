9726321dbeca37f6c424383544774768a2032d40
John Laws Harrison <johnlawsharrison@gmail.com>
2016-07-18 01:00:32 -0700

    Add tradelist service skeleton

--- js/app.js
+++ js/app.js
@@ -16,3 +16,22 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
 //controller for trading list
 app.controller('TradeListCtrl', ['$scope', function ($scope) {
 }]);
+
+//service for managing the state of the tradelist
+app.factory('tradelistService', function() {
+	var service = {}
+
+	if (localStorage['watchlist'] !== undefined) {
+		service.watchlist = JSON.parse(localStorage.watchlist);
+	} else {
+		service.watchlist = [];
+	}
+
+	service.addPost = function(movie) {
+	};
+
+	service.removePost = function() {
+	};
+
+	return service;
+});
