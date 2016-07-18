a6dbff3eba1e0d8c50f8eadfe0b1e9999b8befe6
John Laws Harrison <johnlawsharrison@gmail.com>
2016-07-18 08:30:37 -0700

    Add super nifty user service auth wrapper

--- js/app.js
+++ js/app.js
@@ -19,50 +19,30 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
 }]);
 
 //controller for navbar (logout and search from any part of the app)
-app.controller('NavCtrl', ['$scope', '$firebaseAuth', function ($scope, $firebaseAuth) {
-	var Auth = $firebaseAuth();
-
-	//respond to changes in auth state
-	Auth.$onAuthStateChanged(function(firebaseUser) {
-		if (firebaseUser) {
-			$scope.uid = firebaseUser.uid;
-		} else {
-			$scope.uid = undefined;
-		}
-	});
+app.controller('NavCtrl', ['$scope', 'userService', function ($scope, userService) {
+	$scope.user = userService;
 
 	//log out the signed-in user
 	$scope.logout = function () {
-		Auth.$signOut();
+		userService.auth.$signOut();
 	};
 
 }]);
 
 //controller for trading list
-app.controller('TradeListCtrl', ['$scope', '$firebaseAuth', '$firebaseArray', function ($scope, $firebaseAuth, $firebaseArray) {
+app.controller('TradeListCtrl', ['$scope', '$firebaseArray', 'userService', function ($scope, $firebaseArray, userService) {
 	var baseRef = firebase.database().ref();
 	var tradeListRef = baseRef.child('tradelist');
 	var usersRef = baseRef.child('users');
 
+	$scope.user = userService;
 	$scope.tradelist = $firebaseArray(tradeListRef);
-
-	var Auth = $firebaseAuth();
-
-	//respond to changes in auth state
-	//TODO: find a way to factor out all of these state checks
-	Auth.$onAuthStateChanged(function(firebaseUser) {
-		if (firebaseUser) {
-			$scope.uid = firebaseUser.uid;
-		} else {
-			$scope.uid = undefined;
-		}
-	});
 
 	//list a new trade post on the board
 	$scope.listTrade = function () {
 		//write the post data to database
 		var offerData = {
-			"userid": $scope.uid,
+			"userid": $scope.user.uid,
 			"timestamp": firebase.database.ServerValue.TIMESTAMP,
 			"offering": {
 				"species": $scope.offerSpecies,
@@ -79,23 +59,15 @@ app.controller('TradeListCtrl', ['$scope', '$firebaseAuth', '$firebaseArray', fu
 }]);
 
 //controller for sign up/in page
-app.controller('SignUpCtrl', ['$scope', '$firebaseAuth', function ($scope, $firebaseAuth) {
+app.controller('SignUpCtrl', ['$scope', 'userService', function ($scope, userService) {
 	var baseRef = firebase.database().ref();
 	var usersRef = baseRef.child('users');
-	var Auth = $firebaseAuth();
 
-	//respond to changes in auth state
-	Auth.$onAuthStateChanged(function(firebaseUser) {
-		if (firebaseUser) {
-			$scope.uid = firebaseUser.uid;
-		} else {
-			$scope.uid = undefined;
-		}
-	});
+	$scope.user = userService;
 
 	//create a new user with firebase
 	$scope.signUp = function () {
-		Auth.$createUserWithEmailAndPassword($scope.email, $scope.password)
+		userService.auth.$createUserWithEmailAndPassword($scope.email, $scope.password)
 			.then(function(firebaseUser) {
 				console.log('user created: ' + firebaseUser.uid);
 				var userData = {
@@ -103,6 +75,7 @@ app.controller('SignUpCtrl', ['$scope', '$firebaseAuth', function ($scope, $fire
 				};
 				var newUser = usersRef.child(firebaseUser.uid);
 				newUser.set(userData);
+				userService.username = $scope.username;
 			})
 			.catch(function(error) {
 				console.log(error);
@@ -111,7 +84,35 @@ app.controller('SignUpCtrl', ['$scope', '$firebaseAuth', function ($scope, $fire
 
 	//sign in as the user
 	$scope.signIn = function () {
-		console.log(Auth.$signInWithEmailAndPassword($scope.email, $scope.password));
+		userService.auth.$signInWithEmailAndPassword($scope.email, $scope.password)
+			.then(function() {
+			// attach username to user service upon sign-in
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	};
+}]);
+
+//wrapper service for firebase user auth and maintaining other state for logged-in user
+app.factory('userService', ['$firebaseAuth', function ($firebaseAuth) {
+	var service = {
+		'auth': $firebaseAuth()
 	};
 
+	//respond to changes in auth state
+	service.auth.$onAuthStateChanged(function(firebaseUser) {
+		if (firebaseUser) {
+			// add this users name
+			service.uid = firebaseUser.uid;
+			// add this users list of pokemon
+		} else {
+			// remove everything except auth directive link
+			service.username = undefined;
+			service.pokemon = undefined;
+			service.uid = undefined;
+		}
+	});
+
+	return service;
 }]);
